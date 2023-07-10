0d84a9a38b3db101959cfe3d4beb7f70799506fa
Pablo <pawellreshet@icloud.com>
2023-07-10 08:36:04 +0300

    Initial Commit

---
+++ ToDoList-SwiftUI/ToDoList_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoList_SwiftUIApp.swift
+//  ToDoList-SwiftUI
+//
+//  Created by Pavel Reshetov on 10/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct ToDoList_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
