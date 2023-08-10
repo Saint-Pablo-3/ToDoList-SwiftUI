9741c0d45a45447b6bb23847d32287e5ad4aa785
Pablo <pawellreshet@icloud.com>
2023-08-10 08:04:44 +0300

    date

--- ToDoList-SwiftUI/Views/MainView.swift
+++ ToDoList-SwiftUI/Views/MainView.swift
@@ -12,10 +12,25 @@ struct MainView: View {
     
     var body: some View {
         if viewModel.isSignedIn, !viewModel.currentUserID.isEmpty {
-            // signed in
-            ToDoListView()
+            accountView
+          
         } else {
             LoginView()
+        }
+    }
+    
+    @ViewBuilder
+    var accountView: some View {
+        TabView {
+            ToDoListView(userId: viewModel.currentUserID)
+                .tabItem {
+                    Label("Home", systemImage: "house")
+                }
+            
+            ProfileView()
+                .tabItem {
+                    Label("Profile", systemImage: "person.circle")
+                }
         }
     }
 }
--- ToDoList-SwiftUI/Views/ToDoListItemsView.swift
+++ ToDoList-SwiftUI/Views/ToDoListItemsView.swift
@@ -7,14 +7,14 @@
 
 import SwiftUI
 
-struct ToDoListItemsViews: View {
+struct ToDoListItemsView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
-struct ToDoListItemsViews_Previews: PreviewProvider {
+struct ToDoListItemsView_Previews: PreviewProvider {
     static var previews: some View {
-        ToDoListItemsViews()
+        ToDoListItemsView()
     }
 }
