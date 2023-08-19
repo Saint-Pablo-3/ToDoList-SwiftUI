93694832363deedf8bed66c83b78b3fce8805fe1
Pablo <pawellreshet@icloud.com>
2023-08-19 12:26:47 +0300

    new commit

--- ToDoList-SwiftUI/ViewModels/NewItemViewViewModel.swift
+++ ToDoList-SwiftUI/ViewModels/NewItemViewViewModel.swift
@@ -36,8 +36,7 @@ class NewItemViewViewModel: ObservableObject {
         //Save model
         let db = Firestore.firestore()
         
-        
-        
+    
         db.collection("users")
             .document(uId)
             .collection("todos")
